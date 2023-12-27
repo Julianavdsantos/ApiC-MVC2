a6c84ce227d33293801e80c333d93bb1b1aaf736
Juliana Santos <70862676+Julianavdsantos@users.noreply.github.com>
2023-12-27 10:12:03 -0300

    Update SwapiFilmsResponse.cs

--- Models/SwapiFilmsResponse.cs
+++ Models/SwapiFilmsResponse.cs
@@ -4,7 +4,7 @@ namespace API.Models
 {
     public class SwapiFilmsResponse
     {
-        public List<Films> Results { get; set; }// Adicione uma propriedade para a contagem de pessoas
+        public List<Films> Results { get; set; }// Adicione uma propriedade para a contagem dos filmes , assim aparece uma lista
       
 
     }
