606bdeecfcee987a10bc90bf9da8ee694119a627
Juliana Santos <70862676+Julianavdsantos@users.noreply.github.com>
2023-12-27 10:12:27 -0300

    Update SwapiPlanetsResponse.cs

--- Models/SwapiPlanetsResponse.cs
+++ Models/SwapiPlanetsResponse.cs
@@ -6,7 +6,7 @@ namespace API.Models
 {
     public class SwapiPlanetsResponse
     {
-       public List<Planets> Results { get; set; }// Adicione uma propriedade para a contagem de pessoa
+       public List<Planets> Results { get; set; }
 
     }
 
